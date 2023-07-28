7c46b827ddc1e34bbb82e7e43bbf90ea47a7596a
“atomsiwawut1 <atomsiwawut@gmail.com>
2023-07-29 01:12:55 +0700

    “Update

--- pages/2_🔢_Clash Note.py
+++ pages/2_🔢_Clash Note.py
@@ -5,8 +5,11 @@ from PIL import Image
 import datetime
 
 st.set_page_config(page_title='Clash Issues', page_icon=":1234:", layout='centered')
-
-project_name = st.text_input("Please enter the project name", value="Project")
+css_file="styles/main.css"
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+st.title('Clash Note')
+project_name = st.text_input("Please enter the project name", value="")
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 
