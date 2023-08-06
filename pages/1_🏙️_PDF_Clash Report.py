4a176c98bcbbb95e4f72995eafb45059e371bfdc
“atomsiwawut1 <atomsiwawut@gmail.com>
2023-08-06 14:19:33 +0700

    “Update

--- pages/1_🏙️_PDF_Clash Report.py
+++ pages/1_🏙️_PDF_Clash Report.py
@@ -8,14 +8,13 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.lib.enums import TA_LEFT
 from reportlab.lib.units import inch
-import time
 from PIL import Image as pil_image
+
+import time
+from io import BytesIO
 import os
 
 st.set_page_config(page_title='Generate PDF Report', page_icon=":cityscape:", layout='centered')
-css_file="styles/main.css"
-with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
 pdfmetrics.registerFont(TTFont('Sarabun', r'./Font/THSarabunNew.ttf'))
 pdfmetrics.registerFont(TTFont('Sarabun-Bold', r'./Font/THSarabunNew Bold.ttf'))
@@ -23,15 +22,16 @@ pdfmetrics.registerFont(TTFont('Sarabun-Bold', r'./Font/THSarabunNew Bold.ttf'))
 def main():
     st.title('Clash Report Generator')
     project_name = st.text_input("Enter Project Name:")
-    csv_file = st.file_uploader("Upload CSV", type=['csv'])
 
-    if csv_file is not None:
+    csv_file = st.file_uploader("Upload CSV", type=['csv'])
+    uploaded_images = st.file_uploader("Upload JPEGs", type=['jpg', 'jpeg'], accept_multiple_files=True)
+
+    if csv_file and uploaded_images:
         df = pd.read_csv(csv_file, encoding='utf-8-sig')
         df = df.dropna()
-
         df = df.rename(columns={
             "Clash ID": "Clash ID",
-            "ViewName": "View Name",
+            "View Name": "View Name",
             "Date Found": "Date Found",
             "Main Zone": "Main Zone",
             "Sub Zone": "Sub Zone",
@@ -41,16 +41,29 @@ def main():
             "Description": "Description",
             "Discipline": "Discipline",
             "Assign to": "Assign to",
-            "ImagePath": "Image"
+            "Image": "Image",
+            "ImagePath": "ImagePath"
         })
-
         df["Date Found"] = pd.to_datetime(df["Date Found"]).dt.strftime("%m/%d/%Y")
-        st.table(df.head(3))
+
+        image_dict = {img.name: img for img in uploaded_images}
+
+        # Replace the "Image" column with the filenames for display in Streamlit table
+        df_display = df.copy()
+         # Replace the "Image" column with the actual image objects for processing
+        df["Image"] = df["Image"].apply(lambda x: image_dict.get(x, "Image not found"))
+
+        st.table(df_display.head(3))
 
         if st.button("Generate Report"):
-            output_file = generate_pdf(df, project_name)
+            pdf_data = generate_pdf(df, project_name)
+            st.download_button(
+                label="Download PDF Report",
+                data=pdf_data,
+                file_name=f"{time.strftime('%Y%m%d')}_ClashReport_{project_name}.pdf",
+                mime="application/pdf"
+            )
 
-            st.success(f"PDF report generated. You can download it from [here]({output_file}).")
 
 def generate_pdf(df, project_name):
     class MyDocTemplate(BaseDocTemplate):
@@ -79,21 +92,33 @@ def generate_pdf(df, project_name):
             canvas.setFont("Sarabun-Bold", 10)
             canvas.drawRightString(doc.width + inch, doc.height + inch + 0.75*inch, f"Generated on: {timestamp}")
 
-    pdfmetrics.registerFont(TTFont('Sarabun', r'./Font/THSarabunNew.ttf'))
-    pdfmetrics.registerFont(TTFont('Sarabun-Bold', r'./Font/THSarabunNew Bold.ttf'))
-
     logo_path = r"./Media/1-Aurecon-logo-colour-RGB-Positive.png"
 
-    desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-    output_file = os.path.join(desktop_path, f"{time.strftime('%Y%m%d')}_ClashReport_{project_name}.pdf")
+    #desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+    #output_file = os.path.join(desktop_path, f"{time.strftime('%Y%m%d')}_ClashReport_{project_name}.pdf")
+    #pdf = MyDocTemplate(output_file, pagesize=landscape(A3))
+    output = BytesIO()
+    pdf = MyDocTemplate(output, pagesize=landscape(A3))
 
-    pdf = MyDocTemplate(output_file, pagesize=landscape(A3))
+
 
     styles = getSampleStyleSheet()
     cell_style = styles["Normal"]
     cell_style.fontName = "Sarabun"
     cell_style.alignment = TA_LEFT
 
+    # [Rest of your code remains the same here, except in the table-building section]
+    content = []
+
+    for _, row in df.iterrows():
+        img_data = row['Image']
+    if img_data != "Image not found":
+        image_stream = BytesIO(img_data.read())
+        img_data.seek(0)  # Reset the file pointer
+        img = Image(image_stream, width=150, height=150)
+    else:
+        img = 'Image not found'
+        
     header_style = ParagraphStyle(
         "HeaderStyle",
         parent=styles["Normal"],
@@ -120,17 +145,22 @@ def generate_pdf(df, project_name):
     ]
 
     header_data = [Paragraph(cell, header_style) for cell in df.columns.tolist()]
-    # Manually reorder the header_data list to match the desired column order
     column_order = ["Clash ID", "Image", "View Name", "Date Found", "Main Zone", "Sub Zone", "Level",
                     "Issues Type", "Issues Status", "Description", "Discipline", "Assign to"]
     header_data_reordered = [header_data[df.columns.get_loc(col)] for col in column_order]
     content = []
 
     for _, row in df.iterrows():
-        try:
-            img = Image(row['Image'], width=150, height=150)
-        except FileNotFoundError:
+        img_data = row['Image']
+        if isinstance(img_data, str):  # If it's a string, it's "Image not found"
             img = 'Image not found'
+        else:
+            try:
+                image_stream = BytesIO(img_data.getvalue())  # use getvalue() for file-like object
+                img = Image(image_stream, width=150, height=150)
+            except:
+                img = 'Image not found'
+        
         row_data = [
             Paragraph(str(row["Clash ID"]), cell_style),
             img,
@@ -152,8 +182,9 @@ def generate_pdf(df, project_name):
     table = Table(data, repeatRows=1, style=table_style)
     elems = [Spacer(1, 0.5*inch), table]
     pdf.build(elems)
+    output.seek(0)
+    return output
 
-    return output_file
 
 if __name__ == "__main__":
     main()
